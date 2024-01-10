52513cae56d38617aef124c217799b75df7f888d
Pot <potdiolola14@gmail.com>
2024-01-10 17:48:39 +0800

    new logo

--- script.js
+++ script.js
@@ -98,21 +98,6 @@ tabLogos.forEach((tabLogo, index) => {
 
 
 //HOMEPAGE//
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-});
 
 
 
